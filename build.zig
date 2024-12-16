56ebe8bccbc3d5a3c9d912fe153ad7c5e72c7c52
Xnuk Shuman <admin@xnu.kr>
2024-12-16 19:10:31 +0900

    yay i know how to fix zig version

--- build.zig
+++ build.zig
@@ -1,4 +1,13 @@
 const std = @import("std");
+const zig_version = @import("builtin").zig_version;
+
+comptime {
+	// 0.13.x
+	if (!(zig_version.major == 0 and zig_version.minor == 13)) {
+		@compileError("Your zig version is not compatible with 0.13.x");
+	}
+}
+
 pub fn build(b: *std.Build) void {
 	const exe = b.addExecutable(.{
 		.name = "kime-to",
